783c50afcf4a88ce43abe72136fce2dff10c6a78
spanastasia <anastasibohdan45@gmail.com>
2021-02-18 13:41:09 +0200

    added new class

---
+++ Sources/SwiftPackage/SomeClass.swift
@@ -0,0 +1,14 @@
+//
+//  SomeClass.swift
+//  
+//
+//  Created by Anastasiia Spiridonova on 18.02.2021.
+//
+
+import Foundation
+
+class SomeClass {
+    public func doSmth() {
+        print("Hollo World!")
+    }
+}
